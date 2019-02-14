844bfa3b4858d4ef0fd0fec4965fee205986ef4c
Robin Haentjens <rubysnightcore@gmail.com>
2019-02-14 18:56:50 +0100

    Adding sendJSON function

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const Websocket = require('ws')
 const debug = require('debug')('wso')
 
+Websocket.prototype.sendJSON = function(obj) {
+	try {
+		this.send(JSON.stringify(obj));
+	} catch (e){}
+};
+
 class WssOrigin extends Websocket.Server {
   constructor(opts, origins = [], cb = () => {}) {
     if (!opts.verifyClient && origins.length > 0) {
